88922e567c19e892798b5c6e1137fa98dca8a748
sergey.shmagin@gmail.com <sergey.shmagin@gmail.com>
2025-05-23 17:13:25 +0500

    before first run

--- backend_api/app/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from backend_api.app.routers import gfpgan, fallai_webhook
-
-app = FastAPI()
-
-app.include_router(gfpgan.router)
-app.include_router(fallai_webhook.router)
